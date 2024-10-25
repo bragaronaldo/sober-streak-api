761a9fa40652fdeb8231d82362b797ee14609203
bragaronaldo <ronaldo.braga@outlook.com>
2024-10-24 21:27:36 -0300

    added health_check

--- api/urls.py
+++ api/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
+    path('', health_check),
     path('admin/', admin.site.urls),
     path('auth/', include('user_auth.urls')),
     path('user/', include('user_data.urls')),
